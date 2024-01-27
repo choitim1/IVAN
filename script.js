781e4ec7e7da4edb007c507d6ec16b25de080c5a
Tim <timurtsoi@yahoo.com>
2024-01-27 17:51:23 -0600

    снял комментарии

--- script.js
+++ script.js
@@ -17,21 +17,20 @@ let d = prompt("На сколько оцените его?", "");
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 */
-// for (let i = 0; i < 2; i++) {
-//   const a = prompt("Один из последних просмотренных фильмов?", "");
-//   const b = prompt("На сколько оцените его?", "");
-//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//     personalMovieDB.movies[a] = b;
-//     console.log("done");
-//   } else {
-//     console.log("error");
-//     i--;
-//   }
-// }
-// console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", "");
+  const b = prompt("На сколько оцените его?", "");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+console.log(personalMovieDB);
 if (personalMovieDB.count < 10) {
   alert("маловато");
 } else if (personalMovieDB.count >= 30) {
   alert("ok");
 } else alert("error");
-
